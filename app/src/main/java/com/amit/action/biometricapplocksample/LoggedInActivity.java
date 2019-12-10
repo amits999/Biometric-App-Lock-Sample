a77f3f9e3114b19d94b46a48f4d98cb75e08f302
amits999 <awesomeamit50@gmail.com>
2019-12-11 00:29:48 +0530

    Added New Activity LoggedIN

---
+++ app/src/main/java/com/amit/action/biometricapplocksample/LoggedInActivity.java
@@ -0,0 +1,14 @@
+package com.amit.action.biometricapplocksample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoggedInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in);
+    }
+}
--- app/src/main/java/com/amit/action/biometricapplocksample/MainActivity.java
+++ app/src/main/java/com/amit/action/biometricapplocksample/MainActivity.java
@@ -2,13 +2,17 @@ package com.amit.action.biometricapplocksample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.hardware.biometrics.BiometricPrompt;
+import android.hardware.biometrics.BiometricPrompt.AuthenticationCallback;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
