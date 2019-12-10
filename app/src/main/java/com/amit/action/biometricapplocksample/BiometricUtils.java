e4014eb56ce56c585477a575e02e75b2ec2c3d3f
amits999 <awesomeamit50@gmail.com>
2019-12-11 01:20:10 +0530

    Added Functionality of Authentication.

--- app/src/main/java/com/amit/action/biometricapplocksample/BiometricUtils.java
+++
@@ -1,4 +0,0 @@
-package com.amit.action.biometricapplocksample;
-
-public class BiometricUtils {
-}
--- app/src/main/java/com/amit/action/biometricapplocksample/MainActivity.java
+++ app/src/main/java/com/amit/action/biometricapplocksample/MainActivity.java
@@ -1,11 +1,17 @@
 package com.amit.action.biometricapplocksample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
-import android.hardware.biometrics.BiometricPrompt;
-import android.hardware.biometrics.BiometricPrompt.AuthenticationCallback;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -14,5 +20,50 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Executor executor = Executors.newSingleThreadExecutor();
+
+        final FragmentActivity activity = this;
+
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(activity, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                    // user clicked negative button
+                    Toast.makeText(activity, "Operation Cancelled By User!", Toast.LENGTH_SHORT).show();
+                } else {
+                    //TODO: Called when an unrecoverable error has been encountered and the operation is complete.
+                }
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                //TODO: Called when a biometric is recognized.
+                startActivity(new Intent(MainActivity.this, LoggedInActivity.class));
+                Toast.makeText(activity, "Login Successful!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                //TODO: Called when a biometric is valid but not recognized.
+                Toast.makeText(activity, "Fingerprint not recognized!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Set the title to display.")
+                .setSubtitle("Set the subtitle to display.")
+                .setDescription("Set the description to display")
+                .setNegativeButtonText("Negative Button")
+                .build();
+
+        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
     }
 }
